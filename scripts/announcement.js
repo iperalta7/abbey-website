a475b166d903e87487a042b9484112c3cb28fe15
Isaiah Peralta <98777083+iperalta7@users.noreply.github.com>
2023-02-20 23:20:52 +0000

    update announcements logic

--- scripts/announcement.js
+++ scripts/announcement.js
@@ -16,29 +16,44 @@ function getDate(){
     // This arrangement can be altered based on how we want the date's format to appear.
     let currentDate = `${monthNames[month]} ${day},${year}`;
     console.log(currentDate);
-    return 
+    return  currentDate;
 }
-
-getDate();
 
 //GRABS FORM DATA 
 // RETURNS TITLE AND DETAILS SUBMITTED BY FORM 
 // FOR NEW ANNOUNCEMENT CONTENT
 function getFormData(){
-    const form = document.getElementById("add-announcement");
-    const title = form.elements['title'].value;
-    const details = form.elements['details'].value;
-
+    var form = document.getElementById("add-announcement");
+    var title = form.elements['title'].value;
+    var details = form.elements['details'].value;
+    form.reset();
     console.log(title,details);
 
-    form.reset();
-
-    return title, details;
+    return { title, details }
 };
 
+
+function change_first_announcement(){
+    if(main_content.children.length > 0){
+        var curr_feature_post = main_content.children[3];
+        if(curr_feature_post.nextSibling.className == "row"){
+            curr_feature_post.nextSibling.children[1].id = "";
+        }
+    }
+}
+
+
+/*function for add new nnouncement event listener
+Most of the stuff in this function is just
+for creating new elements and nodes for the new announcement
+*/
 function newAnnouncement(){
-    
-    title,details = getFormData();
+
+
+
+    var data = getFormData();
+    let title = data.title,
+    details = data.details;
 
     //default div for row containement of  announcement post
     var div_row = document.createElement("div");
@@ -53,6 +68,7 @@ function newAnnouncement(){
     //div for actual post
     var post = document.createElement("div");
     post.className = "col d-flex justify-content-center text-center";
+    post.id = "featured-post";
 
     //div for card 
     var card = document.createElement("div");
@@ -64,50 +80,67 @@ function newAnnouncement(){
 
     //img 
     var new_img = document.createElement("img");
+    new_img.className = "card-img-top";
     new_img.src = "https://dummyimage.com/850x350/dee2e6/6c757d.jpg";
     new_img.alt = "...";
+
+    img_container.appendChild(new_img);
+
+    card.appendChild(img_container);
 
     //div for card body
     var card_body = document.createElement("div");
     card_body.className = "card-body";
 
+
     var date = document.createElement("div");
     date.className = "small text-muted";
+    var curr_date_text = getDate();
+    var dateNode = document.createTextNode(curr_date_text);
+    date.appendChild(dateNode);
+    card_body.appendChild(date);
+    
+
 
     var new_title = document.createElement("h2");
     new_title.className = "card-title";
+    var titleNode = document.createTextNode(title);
+    new_title.appendChild(titleNode);
+    card_body.appendChild(new_title);
+
 
     var ptext = document.createElement("p");
     ptext.className = "card-text";
+    var pNode = document.createTextNode(details);
+    ptext.appendChild(pNode);
+    card_body.appendChild(ptext);
+
+    
     var new_btn = document.createElement("a");
     new_btn.className = "btn btn-primary";
     new_btn.href="#!";
+    card_body.appendChild(new_btn);
+
+    card.appendChild(card_body);
+    post.appendChild(card);
+    div_row.appendChild(post);
+
+    var curr_feature_post = main_content.children[3];
+    console.log(curr_feature_post);
+
+     //empty div (bootstrap grid)
+     var empty_col_div2 = document.createElement("div");
+     empty_col_div2.className = "col-2";
+     div_row.appendChild(empty_col_div2);
+
+    main_content.insertBefore(div_row, curr_feature_post)
 
 
-
-
+    //change recent announcement to featured
+    change_first_announcement();
 };
-/*
-        <!-- Featured blog post-->
-        <div class="row"> 
-            <div class="col-2"></div>
-            <div id="featured-post" class="col d-flex justify-content-center text-center">
-                <div class="card mb-4">
-                    <a href="#!"><img class="card-img-top" src="https://dummyimage.com/850x350/dee2e6/6c757d.jpg"
-                            alt="..."></a>
-                    <div class="card-body">
-                        <div class="small text-muted">January 1, 2022</div>
-                        <h2 class="card-title">Dorm Store This Week!</h2>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid
-                            atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero
-                            voluptate voluptatibus possimus, veniam magni quis!</p>
-                        <a class="btn btn-primary" href="#!">Read more →</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-2"></div>
-        </div>
-        */
 
 //event listener to form "submit" button
 newBtn.addEventListener('click', newAnnouncement);
+
+
